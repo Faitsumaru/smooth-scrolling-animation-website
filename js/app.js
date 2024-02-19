85893956fcf449ca7e0badbbd7684f2a226d1b51
Faitsumaru <kiryakov.g.m@gmail.com>
2024-02-19 13:28:53 +0300

    gsap gallery items animation

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ if (ScrollTrigger.isTouch !== 1) { //smooth scroll w/ no touch using devices
         smooth: 1.5,
         effects: true
     })
-}
 
+    
 gsap.fromTo('.hero', { opacity: 1 }, {
     opacity: 0,
     scrollTrigger: {
@@ -18,3 +18,35 @@ gsap.fromTo('.hero', { opacity: 1 }, {
         scrub: true
     }
 })
+
+let itemsLeft = gsap.utils.toArray('.gallery__left .gallery__item')
+
+itemsLeft.forEach(item => {
+    gsap.fromTo(item, { x: -100, opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-750',
+            end: '-150',
+            scrub: true,
+        }
+    })
+})
+
+
+let itemsRight = gsap.utils.toArray('.gallery__right .gallery__item')
+
+itemsRight.forEach(item => {
+    gsap.fromTo(item, { x: 100, opacity: 0 }, {
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-750',
+            end: '-150',
+            scrub: true,
+        }
+    })
+})
+
+
+}
